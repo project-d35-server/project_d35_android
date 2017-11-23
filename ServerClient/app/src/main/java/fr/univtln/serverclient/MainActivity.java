04eda1059bf4ca63d7b54484235c090613f9bbb3
Damien L <damien-lemenager@etud.univ-tln.fr>
2017-11-23 14:40:58 +0100

    Project with AndroidAnnotations 4.3.1 (compatibility Android O)

---
+++ ServerClient/app/src/main/java/fr/univtln/serverclient/MainActivity.java
@@ -0,0 +1,10 @@
+package fr.univtln.serverclient;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import org.androidannotations.annotations.EActivity;
+
+@EActivity(R.layout.activity_main)
+public class MainActivity extends AppCompatActivity {
+}
